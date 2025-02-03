e2bee2163ee14e5105d69f1c725ad9bb8c6f950c
Khilchenko Yurii <yu.kh414@gmail.com>
2025-02-03 03:15:40 +0200

    Add project files.

---
+++ MVCPilotProjectWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCPilotProjectWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
