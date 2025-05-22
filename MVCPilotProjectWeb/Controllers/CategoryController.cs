5f243f09bc982dcdd6edf716c14328ecd00026bb
Khilchenko Yurii <38539304+KhilchenkoYurii@users.noreply.github.com>
2025-05-23 00:50:11 +0300

    Add ApplicationDbContext, connect to db, create and show first db table - Category

---
+++ MVCPilotProjectWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPilotProjectWeb.Data;
+
+namespace MVCPilotProjectWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDb;
+
+        public CategoryController(ApplicationDbContext applicationDb)
+        {
+            _applicationDb = applicationDb;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _applicationDb.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
