5cb845ccfcad3b56811424f202bf24994134f0a7
Khilchenko Yurii <38539304+KhilchenkoYurii@users.noreply.github.com>
2025-09-28 22:48:46 +0300

    Add delayed payment process

--- MVCPilotProjectWeb/Areas/Admin/Controllers/OrderController.cs
+++ MVCPilotProjectWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using MVCPilotProject.Models;
 using MVCPilotProject.Models.ViewModels;
 using MVCPilotProject.Utility;
 using Stripe;
+using Stripe.Checkout;
 using Stripe.Climate;
 using System.Security.Claims;
 
@@ -73,7 +74,66 @@ namespace MVCPilotProjectWeb.Areas.Admin.Controllers
         [HttpPost]
         public IActionResult DetailsPayNow()
         {
-            return RedirectToAction(nameof(Details), new { orderId = OrderVm.OrderHeader.Id });
+            OrderVm.OrderHeader = _unitOfWork.OrderHeader
+                .Get(u => u.Id == OrderVm.OrderHeader.Id, includeParameter: "ApplicationUser");
+            OrderVm.OrderDetails = _unitOfWork.OrderDetail
+                .GetAll(u => u.OrderHeaderId == OrderVm.OrderHeader.Id, includeParameter: "Product");
+
+            var domain = "http://localhost:5193/"; //Request.Scheme + "://" + Request.Host.Value + "/";
+            var options = new SessionCreateOptions
+            {
+                SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={OrderVm.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/order/details?orderId={OrderVm.OrderHeader.Id}",
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+            };
+
+            foreach (var item in OrderVm.OrderDetails)
+            {
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Title
+                        }
+                    },
+                    Quantity = item.Count
+                };
+                options.LineItems.Add(sessionLineItem);
+            }
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            _unitOfWork.OrderHeader.UpdateStripePaymentId(OrderVm.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+
+            Response.Headers.Add("Location", session.Url);
+
+            return new StatusCodeResult(303);
+        }
+
+        public IActionResult PaymentConfirmation(int orderHeaderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(o => o.Id == orderHeaderId);
+            if (orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+            {
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeader.UpdateStripePaymentId(orderHeaderId, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeader.UpdateStatus(orderHeaderId, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+                    _unitOfWork.Save();
+                }
+            }
+
+            return View(orderHeaderId);
         }
 
         [HttpPost]
